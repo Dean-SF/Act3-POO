b47ff22b4cbdf42719f7cef3ef50739b406d48a3
Dean-SF <85375794+Dean-SF@users.noreply.github.com>
2021-08-25 08:30:37 -0600

    Archivos de la actividad agregados

---
+++ Codigo de java/src/miprimerobjetog03/Curso.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerobjetog03;
+
+/**
+ *
+ * @author ersolano
+ */
+public class Curso {
+    
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private int horas;
+    private boolean asistenciaObligatoria;
+    
+    private final static double NOTA_APROBACION = 67.5;
+    // private/public es un modificador de acceso que define la visibilidad del atributo
+    // static es un modificador de acceso que define un atributo global
+    // final es un modificador de acceso que define un atributo constante
+    
+    public Curso(){
+        // constructor default
+    }
+
+    public Curso(String codigo, String nombre, int p_creditos) {
+        // constructor sobrecargado
+        this.codigo = codigo;
+        this.nombre = nombre;
+        creditos = p_creditos;
+    }
+    
+    public Curso(int p_creditos, String codigo, String nombre) {
+        // constructor sobrecargado con igual cantidad de parámetros, distinto orden
+        this.codigo = codigo;
+        nombre = nombre;
+        creditos = p_creditos;
+    }
+
+    public Curso(String codigo, String nombre, int creditos, int horas, boolean asistenciaObligatoria) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.horas = horas;
+        this.asistenciaObligatoria = asistenciaObligatoria;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public boolean isAsistenciaObligatoria() {
+        return asistenciaObligatoria;
+    }
+
+    public void setAsistenciaObligatoria(boolean asistenciaObligatoria) {
+        this.asistenciaObligatoria = asistenciaObligatoria;
+    }
+
+    public static double getNOTA_APROBACION() {
+        return NOTA_APROBACION;
+    }
+    
+    
+    
+   
+    @Override
+    public String toString() {
+        return "Curso{" + "codigo=" + codigo + ", nombre=" + nombre + 
+               ", creditos=" + creditos + ", horas=" + horas + 
+               ", asistenciaObligatoria=" + asistenciaObligatoria + '}';
+    }
+    
+    
+    
+}
---
+++ Codigo de java/src/miprimerobjetog03/PruebaCursos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerobjetog03;
+
+/**
+ *
+ * @author ersolano
+ */
+public class PruebaCursos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo soy nuevo en Java");
+        System.out.println("Me llamo Ericka");
+        
+        int cantidad = 0;
+        boolean seguir = false;
+        double precio = 0;
+
+        String nombre = null;
+        Curso miCurso = null;   // declaración de una instancia, variable de tipo Curso
+        miCurso = new Curso("Programacion Orientada a Objetos", "IC2101", 3);   //  instanciación del objeto
+        
+        Curso otroCurso = new Curso();   // declaracion e instanciacion del objeto Curso
+        otroCurso.setCodigo("IC2001");
+        otroCurso.setNombre("Estructuras de Datos");
+        otroCurso.setCreditos(4);
+        otroCurso.setHoras(4);
+        otroCurso.setAsistenciaObligatoria(true);
+        
+        System.out.println(Curso.getNOTA_APROBACION());
+                
+        System.out.println(miCurso.getCodigo() + "-" + miCurso.getNombre() + "-" + miCurso.getCreditos());
+                
+        System.out.println(otroCurso);
+        
+    }
+    
+}
